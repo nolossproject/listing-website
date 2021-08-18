37ab6f2d79727ef46b7e7fb89ca5deca0046c218
Ivan Mihajlovic <develop@nolossproject.com>
2021-08-18 19:33:45 +0200

    Add site layout components

---
+++ resources/views/partials/footer.php
@@ -0,0 +1,25 @@
+<footer>
+  <ul class="nav">
+    <li>Useful links</li>
+
+    <li class="social my-4">
+      <img src="assets/logo_telegram.svg" alt="">
+      <img src="assets/logo_no_loss.svg" alt="">
+      <img src="assets/logo_twitter.svg" alt="">
+    </li>
+
+    <li>Disclaimer</li>
+    <li>Privacy Policy</li>
+    <li>Terms & conditions</li>
+  </ul>
+  <div class="partners my-5">
+    <h4>Partners</h4>
+    <ul>
+      <li><img src="assets/partners/coin_gecko.svg" alt=""></li>
+      <li><img src="assets/partners/pancake_swap.svg" alt=""></li>
+    </ul>
+  </div>
+  <div class="p-2">
+    No Loss project &copy; <?php echo date('Y') ?> - contact@noloss.app
+  </div>
+</footer>
---
+++ resources/views/partials/header.php
@@ -0,0 +1,30 @@
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark">
+  <div class="container-fluid px-0">
+    <a class="navbar-brand flex-grow-1" href="/"><img class="d-xl-none mr-3" src="assets/logo_no_loss.svg" height="30"/><span>No Loss </span>listing</a>
+    <img class="d-none d-xl-inline logo" src="assets/logo_no_loss.svg" height="35"/>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-end flex-grow-1" id="navbarSupportedContent">
+      <ul class="d-flex navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/apply">Apply</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Get approved</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Promote</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Partners</a>
+        </li>
+        <li class="nav-item">
+          <a class="btn btn-success px-4" href="#" role="button">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
